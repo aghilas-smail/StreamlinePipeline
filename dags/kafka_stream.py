0dd945a46373c2413aa27c01d7acbe4f8e877c7b
Smail <aghilas.smail@gmail.com>
2024-11-18 14:03:12 +0100

    test api work!

--- dags/kafka_stream.py
+++ dags/kafka_stream.py
@@ -1,14 +1,23 @@
 from datetime import datetime
-from airflow import DAG
-from airflow.operators.python import PythonOperator
+# from airflow import DAG
+# from airflow.operators.python import PythonOperator
 
 default_args = {
     'owner': 'airscholar', 
     'start_date': datetime(2024, 11, 14)
 }
 
-with DAG('user_automation', 
-         default_args=default_args,
-         schedule_interval='@daily',
-         # Le catchup signifie on prend pas en consédiration les exécutions manquantes 
-         catchup=False) as dag:
+def stream_data():
+    import json
+    import requests
+    
+    res = requests.get("https://randomuser.me/api/")
+    print(res.json())
+
+# with DAG('user_automation', 
+#          default_args=default_args,
+#          schedule_interval='@daily',
+#          # Le catchup signifie on prend pas en consédiration les exécutions manquantes 
+#          catchup=False) as dag:
+
+stream_data()
